2b3c7309a1dc472869b8dfbdd4594bd3fc42a478
bumbot <pkim1295@gmail.com>
2020-02-11 17:46:49 -0500

    added our logo lol

--- bin/run.rb
+++ bin/run.rb
@@ -8,6 +8,24 @@ require_relative '../db/seeds.rb'
 # Welcome to DebtFlix
 def openingWelcome
     puts "\n\nWelcome to DebtFlix\n\n"
+
+    puts " ________        _________     ________    ________________   "
+    puts "|         \\     |   ______|   |   __   \\  |_____     ______|  "
+    puts "|   ____   \\    |  |          |  |   \\  |       |   |         "
+    puts "|  |    \\   \\   |  |_____     |  |___/  /       |   |         "
+    puts "|  |     |   |  |   _____|    |   _____/__      |   |         "
+    puts "|  |     |   |  |  |          |   ____    \\     |   |         "
+    puts "|  |_____/  /   |  |_______   |  [____]   |     |   |         "
+    puts "|_________ /    |__________|  |___________/     |___|         "
+    puts " ___________   ___         __________     ___        ___            "
+    puts "|           | |   |       |___    ___|   |   \\      /   |     "
+    puts "|   ________| |   |           |  |        \\   \\    /   /      "
+    puts "|  |______    |   |           |  |         \\   \\__/   /       "
+    puts "|   ______|   |   |           |  |          \\   __   /        "
+    puts "|  |          |   |           |  |         /   /  \\   \\       "
+    puts "|  |          |   |______  ___|  |____    /   /    \\   \\      "
+    puts "|__|          |__________||___________|  |___/      \\___|     "
+
     puts "\nPress \"y\" to continue or type \"exit\" to terminate the program.\n\n\n"
     print "Input: "
     input = gets.chomp.downcase.gsub(" ", "")
